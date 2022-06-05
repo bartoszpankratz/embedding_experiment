fdd6772188eeac19739cc5adaee63e174d8b262d
bartoszpankratz <bartosz.pankratz@gmail.com>
2022-06-05 02:32:28 -0400

    files 2nd experiment

---
+++ run_clustering_rl.jl
@@ -0,0 +1,187 @@
+#run_clustering.jl embed_file g_name g_cl_name res_dir seed 
+
+julia_path = "julia-1.7.2/bin/julia"
+no_iters = 10
+embed_file = ARGS[1]
+params = split(embed_file[1:end-4], "_")
+embed_name = split(params[1],"/")[end]
+graph_name = params[2]
+dims = parse(Int,params[3])
+
+embed_params = ""
+if embed_name == "node2vec" 
+    embed_params = "p: $(params[4]), q: $(params[5])"
+elseif embed_name == "SDNE"
+    embed_params = "ℓ: $(params[4]), β: $(params[5])"
+elseif embed_name == "GraRep"
+    embed_params = "k: $(params[4])"
+elseif embed_name == "HOPE"
+    embed_params = params[4]
+end
+
+
+
+g_name = ARGS[2]
+g_cl_name = ARGS[3]
+res_dir = ARGS[4]
+seed = parse(Int, ARGS[5])
+
+using Random, Base.Iterators
+using PyCall, Clustering
+ 
+Random.seed!() = seed
+
+nx = pyimport("networkx");
+ig = pyimport("igraph");
+community_louvain = pyimport("community"); 
+la = pyimport("leidenalg");
+gmm = pyimport("sklearn.mixture")
+hdbscan = pyimport("hdbscan")
+
+
+#create results path:
+mkpath(res_dir)
+
+#read graph:
+G = ig.Graph.Read_Edgelist(g_name, directed = false)
+G.delete_vertices(0)
+g = nx.read_edgelist(g_name)
+nodeslist = collect(nx.nodes(g));
+n = length(nodeslist)
+
+gt_partition = [parse.(Int,e2) - 1 for (e1,e2) in split.(readlines(g_cl_name))]
+
+#Read embedding:
+embed = transpose(reduce(hcat,[parse.(Float64,x) for x in split.(readlines(embed_file)[2:end])]))
+embed = embed[sortperm(embed[:, 1]), :][:,2:end];
+
+
+#embedding parameters:
+
+#K-Means and Gaussian Mixture Model:
+n_clusters = n_clusters = [Int(ceil(n/k)) for k in 2:2:32]
+
+#HDBSCAN:
+min_samples = 1:10
+
+
+
+#CGE score:
+CGE_score = readchomp(`$julia_path CGE_CLI.jl -g $(g_name) -e $(embed_file) -c $(g_cl_name) --seed $(seed)`)
+CGE_score = [parse(Float64, x)  for x in split.(CGE_score[2:end-1], ", ")]
+
+#Mini Batch K-Means:
+for k in n_clusters
+    measures = Dict("louvain_modularity" => [],
+                    "louvain_ami" => [],
+                    "leiden_modularity" => [],
+                    "leiden_ami" => [])
+    labels = kmeans(transpose(embed),k).assignments
+    d = Dict(nodeslist[i] => labels[i] for i = 1:length(labels))
+    for i = 1:no_iters
+        #louvain:
+        louvain_partition = community_louvain.best_partition(g,d,random_state = i)
+        louvain_modularity = community_louvain.modularity(louvain_partition,g)
+        push!(measures["louvain_modularity"], louvain_modularity)
+        louvain_partition =  [louvain_partition["$j"] + 1 for j = 1:length(louvain_partition)]
+        louvain_ami = mutualinfo(gt_partition, louvain_partition)
+        push!(measures["louvain_ami"], louvain_ami)
+        
+        #leiden:
+        leiden_partition = la.find_partition(G, la.ModularityVertexPartition,initial_membership = labels, seed = i)
+        leiden_modularity = G.modularity(leiden_partition.membership)
+        push!(measures["leiden_modularity"], leiden_modularity)
+        leiden_ami = mutualinfo(gt_partition, leiden_partition.membership)
+        push!(measures["leiden_ami"], leiden_ami)
+    end
+    for algo in ["louvain", "leiden"]
+        for measure in ["modularity", "ami"]
+            res_name = res_dir * "results_$(algo)_$(measure)_$(graph_name).dat"
+            resline = vcat([embed_name, dims, embed_params, "K-Means", "k: $(k)"], 
+                CGE_score, measures["$(algo)_$(measure)"])
+            open(res_name, "a") do io
+                println(io, join(resline,";"))
+            end
+        end
+    end
+end
+
+
+#HDBSCAN
+for ms in min_samples
+    measures = Dict("louvain_modularity" => [],
+                    "louvain_ami" => [],
+                    "leiden_modularity" => [],
+                    "leiden_ami" => [])
+    c = hdbscan.HDBSCAN(min_cluster_size=2, min_samples=ms).fit(embed)
+    remain_clusters =  collect((length(unique(c.labels_)) - 1):length(c.labels_))
+    labels = [c.labels_[i] != -1  ? c.labels_[i] : popfirst!(remain_clusters)
+              for i = 1:length(c.labels_)]
+    d = Dict(nodeslist[i] => labels[i] for i = 1:length(labels))
+    for i = 1:no_iters
+        #louvain:
+        louvain_partition = community_louvain.best_partition(g,d,random_state = i)
+        louvain_modularity = community_louvain.modularity(louvain_partition,g)
+        push!(measures["louvain_modularity"], louvain_modularity)
+        louvain_partition =  [louvain_partition["$j"] + 1 for j = 1:length(louvain_partition)]
+        louvain_ami = mutualinfo(gt_partition, louvain_partition)
+        push!(measures["louvain_ami"], louvain_ami)
+        
+        #leiden:
+        leiden_partition = la.find_partition(G, la.ModularityVertexPartition,initial_membership = labels, seed = i)
+        leiden_modularity = G.modularity(leiden_partition.membership)
+        push!(measures["leiden_modularity"], leiden_modularity)
+        leiden_ami = mutualinfo(gt_partition, leiden_partition.membership)
+        push!(measures["leiden_ami"], leiden_ami)
+    end
+    for algo in ["louvain", "leiden"]
+        for measure in ["modularity", "ami"]
+            res_name = res_dir * "results_$(algo)_$(measure)_$(graph_name).dat"
+            resline = vcat([embed_name, dims, embed_params, "HDBSCAN", "min_samples: $(ms)"], 
+                CGE_score, measures["$(algo)_$(measure)"])
+            open(res_name, "a") do io
+                println(io, join(resline,";"))
+            end
+        end
+    end
+end
+
+
+#Gaussian Mixture Models
+for k in n_clusters
+    measures = Dict("louvain_modularity" => [],
+                    "louvain_ami" => [],
+                    "leiden_modularity" => [],
+                    "leiden_ami" => [])
+    c = gmm.GaussianMixture(n_components=k, random_state = seed).fit(embed)
+    labels = c.predict(embed)
+    d = Dict(nodeslist[i] => labels[i] for i = 1:length(labels))
+    for i = 1:no_iters
+        #louvain:
+        louvain_partition = community_louvain.best_partition(g,d,random_state = i)
+        louvain_modularity = community_louvain.modularity(louvain_partition,g)
+        push!(measures["louvain_modularity"], louvain_modularity)
+        louvain_partition =  [louvain_partition["$j"] + 1 for j = 1:length(louvain_partition)]
+        louvain_ami = mutualinfo(gt_partition, louvain_partition)
+        push!(measures["louvain_ami"], louvain_ami)
+        
+        #leiden:
+        leiden_partition = la.find_partition(G, la.ModularityVertexPartition,initial_membership = labels, seed = i)
+        leiden_modularity = G.modularity(leiden_partition.membership)
+        push!(measures["leiden_modularity"], leiden_modularity)
+        leiden_ami = mutualinfo(gt_partition, leiden_partition.membership)
+        push!(measures["leiden_ami"], leiden_ami)
+    end
+    for algo in ["louvain", "leiden"]
+        for measure in ["modularity", "ami"]
+            res_name = res_dir * "results_$(algo)_$(measure)_$(graph_name).dat"
+            resline = vcat([embed_name, dims, embed_params, "GMM", "k: $(k)"], 
+                CGE_score, measures["$(algo)_$(measure)"])
+            open(res_name, "a") do io
+                println(io, join(resline,";"))
+            end
+        end
+    end
+end
+
+
